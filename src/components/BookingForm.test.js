67efb0f816d5f8cf43339e509990bd63cb3280b2
Chitesh <dv.chitesh97@gmail.com>
2025-07-08 20:22:05 +0530

    Reverting changes

--- src/components/BookingForm.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import BookingForm from "./BookingForm";
-
-test('Renders the BookingForm component title', () => {
-  render(<BookingForm />);
-  const title = screen.getByText("Reserve a table");
-  expect(title).toBeInTheDocument();
-});
--- src/components/UnderConstruction.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function UnderConstruction() {
-  return (
-   <section className="under-construction">
-     <h1> Under Construction </h1>
-   </section>
-  )
-}
